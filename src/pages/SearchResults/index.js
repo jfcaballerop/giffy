76837b4c4f57be416505f5d798dc7efe60391406
jfcaballerop <jfcaballerop@gmail.com>
2021-10-28 21:15:37 +0200

    v5 min 20

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Spinner from 'components/Spinner'
 import ListOfGifs from 'components/ListOfGifs'
 import { useGifs } from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
 
 export default function SearchResults({ params }) {
     const { keyword } = params
     const { loading, gifs, setpage } = useGifs({ keyword })
+    const { externalRef } = useRef()
+    const { isNearScreen } = useNearScreen({ externalRef: loading ? null : externalRef })
 
-    const handleNextPage = () => setpage(prevPage => prevPage + 1)
+    // const handleNextPage = () => setpage(prevPage => prevPage + 1)
 
+    const handleNextPage = () => console.log('next page')
+
+    useEffect(() => {
+        if (isNearScreen) handleNextPage()
+
+    })
 
     return <>
         {loading
@@ -18,9 +27,10 @@ export default function SearchResults({ params }) {
                     {decodeURI(keyword)}
                 </h3>
                 <ListOfGifs gifs={gifs} />
+                <div id="visor" ref={externalRef}></div>
             </>
         }
         <br />
-        <button onClick={handleNextPage}>Get next page</button>
+        {/* <button onClick={handleNextPage}>Get next page</button> */}
     </>
 }
