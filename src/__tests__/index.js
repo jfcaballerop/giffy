3493fd912f0c6a2017b4aa4dd09c2c4f78219dfa
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2021-11-04 09:02:51 +0100

    add tests folder

---
+++ src/__tests__/index.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders without crashing', () => {
+    render(<App />);
+    const title = screen.getByText(/Última búsqueda/i);
+    expect(title).toBeInTheDocument();
+});
