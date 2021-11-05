b90758086cb2159176f842bdfde1a629d6a29cdc
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2021-11-05 12:00:45 +0100

    First tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
-test('renders without crashing', () => {
+test('renders without crashing', async () => {
     render(<App />);
-    const title = screen.getByText(/Última búsqueda/i);
+    const title = await screen.findByText(/Última búsqueda/i);
     expect(title).toBeInTheDocument();
 });
+
+test('Home work as expected', async () => {
+    const { container } = render(<App />)
+    await waitFor(() => {
+        expect(container.querySelector('.Gif-link')).toBeInTheDocument()
+
+    }
+    )
+});
+
+test.only('search button when input', async () => {
+    render(<App />)
+    const input = await screen.findAllByRole('textbox')
+    const button = await screen.findAllByRole('button')
+
+    fireEvent.change(input, { target: { value: 'Matrix' } })
+    fireEvent.click(button)
+
+    const title = await screen.findAllByText('Matrix')
+    expect(title).toBeVisible()
+})
