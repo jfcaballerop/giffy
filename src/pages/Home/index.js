b6e26e4691590dfc86eb6545be78d395f6efd2cb
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2021-11-10 09:15:22 +0100

    v6 min 25

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,21 @@
-import React, { useState } from "react"
+import React from "react"
 import { useLocation } from "wouter"
 import ListOfGifs from 'components/ListOfGifs'
 import { useGifs } from 'hooks/useGifs'
 import TrendingSearches from 'components/TrendingSearches'
+import SearchForm from "components/SearchForm"
 
 export default function Home() {
-    const [keyword, setKeyword] = useState('')
     const [path, pushLocation] = useLocation()
     const { loading, gifs } = useGifs()
 
-    const handleSubmit = evt => {
-        evt.preventDefault()
-        // navegar a otra ruta
+    const handleSubmit = ({ keyword }) => {
         pushLocation(`/search/${keyword}`)
-    }
-
-    const handleChange = evt => {
-        setKeyword(evt.target.value)
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <button>Buscar</button>
-                <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
-            </form>
+            <SearchForm onSubmit={handleSubmit} />
             <div className="App-main">
                 <div className="App-results">
                     <h3 className="App-title">Última búsqueda</h3>
