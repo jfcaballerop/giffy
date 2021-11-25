9bad16717792abfbe7ade8ec6251bf02b6632a8f
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2021-11-25 09:01:27 +0100

    add memo deep example v6 min1.18

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -3,14 +3,18 @@ import { Link } from 'wouter'
 import './Gif.css'
 
 
-export default function Gif({ id, title, url }) {
-    return (
-        <div className="Gif">
-            <Link to={`/detail/${id}`} className='Gif-link'>
-                <h4>{title}</h4>
-                <img loading='lazy' alt={title} src={url} />
-            </Link>
-        </div>
+function Gif({ id, title, url }) {
+	return (
+		<div className="Gif">
+			<Link to={`/detail/${id}`} className='Gif-link'>
+				<h4>{title}</h4>
+				<img loading='lazy' alt={title} src={url} />
+			</Link>
+		</div>
 
-    )
+	)
 }
+
+export default React.memo(Gif, (prevProps, nextProps) => {
+	return prevProps.id === nextProps.id
+})
--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -3,16 +3,17 @@ import Gif from '../Gif';
 import './styles.css'
 
 export default function ListOfGifs({ gifs }) {
-    return <div className='ListOfGifs'>
-        {
-            gifs.map(({ id, title, url }) =>
-                <Gif
-                    id={id}
-                    key={id}
-                    title={title}
-                    url={url}
-                />
-            )
-        }
-    </div>
+	return <div className='ListOfGifs'>
+		{
+			gifs.map(({ id, title, url, ...restOfGif }) =>
+				<Gif
+					id={id}
+					key={id}
+					title={title}
+					url={url}
+					extraInfo={restOfGif}
+				/>
+			)
+		}
+	</div>
 }
